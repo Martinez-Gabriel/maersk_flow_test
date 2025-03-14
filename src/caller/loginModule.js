2a699c9c9080d1ac6328a3312b4a43c07a9ac6bb
Gabriel Martinez <mart.gabriel96@gmail.com>
2025-03-14 12:27:26 -0300

    fix(loginModule.js): add logPath parameter

--- src/caller/loginModule.js
+++ src/caller/loginModule.js
@@ -1,39 +1,36 @@
-import dotenv from 'dotenv';
-import {logToCSV} from './utils/logsModule.js'
+import { logToCSV } from '../utils/logsModule.js';
 
-dotenv.config();
-
-export async function iniciarSesion(page, url, username, password) {
+export async function iniciarSesion(page, logPath, url, username, password) {
     const inputUsuario = page.locator('input[name="username"]');
     const inputContrasena = page.locator('input[name="password"]');
     const botonLogin = page.locator('button[type="submit"]');
 
     try {
         console.log("Iniciando sesión...");
-        logToCSV('StartSession', 'Iniciando sesión');
+        logToCSV(logPath, 'StartSession', 'Iniciando sesión');
 
         await page.goto(url + '/caller/');
-        logToCSV('Navigation', 'Página cargada correctamente.');
+        logToCSV(logPath, 'Navigation', 'Página cargada correctamente.');
 
         if (await inputUsuario.isVisible()) {
             await inputUsuario.focus();
             await inputUsuario.fill(username);
             await page.keyboard.press('Tab');
-            logToCSV('InputAction', 'Usuario ingresado correctamente.');
+            logToCSV(logPath, 'InputAction', 'Usuario ingresado correctamente.');
         } else {
             console.error('Campo de usuario no visible.');
         }
 
         if (await inputContrasena.isVisible()) {
             await inputContrasena.fill(password);
-            logToCSV('InputAction', 'Contraseña ingresada correctamente.');
+            logToCSV(logPath, 'InputAction', 'Contraseña ingresada correctamente.');
         } else {
             console.error('Campo de contraseña no visible.');
         }
 
         if (await botonLogin.isVisible()) {
             await botonLogin.click();
-            logToCSV('ButtonClick', 'Botón de inicio de sesión clickeado.');
+            logToCSV(logPath, 'ButtonClick', 'Botón de inicio de sesión clickeado.');
         } else {
             console.error('Botón de inicio de sesión no visible.');
         }
@@ -42,6 +39,6 @@ export async function iniciarSesion(page, url, username, password) {
 
     } catch (error) {
         console.error(`Error al iniciar sesión: ${error.message}`);
-        logToCSV('Error', `Error al iniciar sesión: ${error.message}`);
+        logToCSV(logPath, 'Error', `Error al iniciar sesión: ${error.message}`);
     }
 }
